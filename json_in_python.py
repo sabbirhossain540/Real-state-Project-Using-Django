6d2500b2b62c0252c995ec38bf3515108848e1e3
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-11 12:24:42 +0600

    First Commit

---
+++ json_in_python.py
@@ -0,0 +1,10 @@
+
+import json
+
+userJson = '{"firstname": "Sabbir", "lastname": "Hossain", "age": 30}'
+user = json.loads(userJson)
+print(user)
+
+car = {'make': 'Toyota', 'model':'Allion', 'year': 2020}
+carJSON = json.dumps(car)
+print(carJSON)
