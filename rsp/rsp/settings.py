c9cd8a6d1fe1453f65eaea72d7726f6d7256bd84
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-13 16:57:28 +0600

    Day 3

--- rsp/rsp/settings.py
+++ rsp/rsp/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig'
+
 ]
 
 MIDDLEWARE = [
--- rsp/rsp/urls.py
+++ rsp/rsp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('',include('pages.urls')),
+    path('listings/',include('listings.urls')),
     path('admin/', admin.site.urls),
 
 ]
