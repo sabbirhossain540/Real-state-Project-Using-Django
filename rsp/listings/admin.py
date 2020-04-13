f0c5c52f81257b2c74ba7d15a8be728aaad6041a
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-13 19:47:04 +0600

    Day 3

--- rsp/listings/admin.py
+++ rsp/listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Listing
+
+admin.site.register(Listing)
--- rsp/realtors/admin.py
+++ rsp/realtors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Realtor
+
+admin.site.register(Realtor)
