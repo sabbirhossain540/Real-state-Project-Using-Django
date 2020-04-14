78ef8f8535c06a5152204d795a5fdd1ca6f05bed
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-14 20:46:37 +0600

    Day 4

--- rsp/listings/views.py
+++ rsp/listings/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+#Load Model
+from .models import Listing
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings' : listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
