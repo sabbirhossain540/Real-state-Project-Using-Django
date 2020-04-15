503562504cf8c61f909436499cccfc7bf29a8e02
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-15 17:21:12 +0600

    Day 5

---
+++ rsp/listings/choices.py
@@ -0,0 +1,82 @@
+
+bedroom_choices = {
+    '1' :1,
+    '2' :2,
+    '3' :3,
+    '4' :4,
+    '5' :5,
+    '6' :6,
+    '7' :7,
+    '8' :8,
+    '9' :9,
+    '10' :10
+}
+
+
+price_choices = {
+    '100000' : '$100,000',
+    '200000' : '$200,000',
+    '300000' : '$300,000',
+    '400000' : '$400,000',
+    '500000' : '$500,000',
+    '600000' : '$600,000',
+    '700000' : '$700,000',
+    '800000' : '$800,000',
+    '900000' : '$900,000',
+    '1000000' : '$1M+'
+}
+
+
+state_choices = {
+    'AK': 'Alaska',
+    'AZ': 'Arizona',
+    'AR': 'Arkansas',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DE': 'Delaware',
+    'DC': 'District Of Columbia',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'HI': 'Hawaii',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'IA': 'Iowa',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'ME': 'Maine',
+    'MD': 'Maryland',
+    'MA': 'Massachusetts',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MS': 'Mississippi',
+    'MO': 'Missouri',
+    'MT': 'Montana',
+    'NE': 'Nebraska',
+    'NV': 'Nevada',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NY': 'New York',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VT': 'Vermont',
+    'VA': 'Virginia',
+    'WA': 'Washington',
+    'WV': 'West Virginia',
+    'WI': 'Wisconsin',
+    'WY': 'Wyoming'
+}
+
--- rsp/listings/views.py
+++ rsp/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import bedroom_choices, state_choices, price_choices
 
 #Load Model
 from .models import Listing
@@ -30,4 +31,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'bedroom_choices' : bedroom_choices,
+        'state_choices' : state_choices,
+        'price_choices' : price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
