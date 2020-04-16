66467bcd423334ae8cff14eba185e820dc1ee058
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-16 18:39:44 +0600

    Day 6

--- rsp/accounts/views.py
+++ rsp/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib import auth
+#from django.contrib import messages
 
 # Create your views here.
 
@@ -9,8 +11,39 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        message.error(request, 'Testing error Message')
-        return redirect('register')
+        #message.error(request, 'Testing error Message')
+
+        # Get From values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #Check if Password match
+        if password == password2:
+            #Check User
+            if User.objects.filter(username=username).exists():
+                print("Username Already Taken")
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    print("That email is being used")
+                    return redirect('register')
+                else:
+                    #Look Good
+                    user = User.objects.create_user(username=username, password=password, email=email,first_name=first_name,last_name=last_name)
+                    user.save()
+                    print("User Save Successfully")
+                    return redirect('login')
+                    #Login after registration
+                    #auth.login(request, user)
+            
+        else:
+            print("Password and Password2 Can not match")
+            return redirect('register')
+
     else:
         return render(request,'accounts/register.html')
 
--- rsp/rsp/settings.py
+++ rsp/rsp/settings.py
@@ -147,8 +147,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-#Messages
-from django.contrib.messages import constants as messages
-MESSAGE_TAGS = {
-    messages.ERROR: 'danger'
-}
+# #Messages
+# from django.contrib.messages import constants as messages
+# MESSAGE_TAGS = {
+#     messages.ERROR: 'danger'
+# }
