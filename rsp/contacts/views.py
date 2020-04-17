fa51a0a681e7fc20e5347e2501f93b4f457b7478
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-17 19:47:15 +0600

    Day 7

--- rsp/contacts/views.py
+++ rsp/contacts/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 # Create your views here.
 def contact(request):
-    return
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+        contact.save()
+        print("Data Submitted Successfully")
+        return redirect('/listings/'+listing_id)
+
+
