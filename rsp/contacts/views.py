021b0b233b2b30dda1c12a88e5c09523c825e78a
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-04-17 20:41:53 +0600

    Day 7

--- rsp/contacts/views.py
+++ rsp/contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Contact
+from django.core.mail import send_mail
 
 # Create your views here.
 def contact(request):
@@ -13,8 +14,29 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        #Check if user has made inquery already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                print("You have already made a enquery")
+                return redirect('/listings/'+listing_id)
+
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
+
+        #Send Email
+        send_mail(
+            'Property Listing Inquery',
+            'There has been an inquery for '+ listing + '. Sign into the admin panel for more info',
+            'ashes545454@gmail.com', #Host mail
+            ['job.sabbirhossain308@gmail.com'], #Where We want to sent mail
+            fail_silently=False
+        )
+
+
+
         print("Data Submitted Successfully")
         return redirect('/listings/'+listing_id)
 
